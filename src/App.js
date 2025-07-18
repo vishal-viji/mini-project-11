e925f089d3c462638d70174b1beb102751c0dbfe
vishal-viji <vishalshanmugam928@gmail.com>
2025-07-18 21:04:24 +0530

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ function App() {
   const [error, setError] = useState(null);
   const [showModal, setShowModal] = useState(false);
 
-  const apiKey ='http://www.omdbapi.com/?i=tt3896198&apikey=ae491b2e'
+  const apiKey = "ae491b2e";
 
   const searchMovies = async () => {
     try {
       const response = await axios.get(
-        `http://www.omdbapi.com/?s=${query}&apiKey=http://www.omdbapi.com/?i=tt3896198&apikey=ae491b2e`
+        `http://www.omdbapi.com/?s=${query}&apiKey=ae491b2e`
       );
       setMovies(response.data.Search || []);
     } catch (error) {
@@ -36,7 +36,7 @@ function App() {
   const getMovieDetails = async (id) => {
     try {
       const response = await axios.get(
-        `http://www.omdbapi.com/?i=${id}&apiKey=http://www.omdbapi.com/?i=tt3896198&apikey=ae491b2e`
+        `http://www.omdbapi.com/?i=${id}&apiKey=ae491b2e`
       );
       setSelectedMovie(response.data);
     } catch (error) {
@@ -52,86 +52,89 @@ function App() {
 
   return (
     <>
-      {apiKey == "" && (
-        <h1>
+      {/* <h1>
           Please visit to this and create your apikey and paste in this app.js
           file
           <a href="https://www.omdbapi.com/apikey.aspx" target="_blank">Click</a>
-        </h1>
-      )}
-      {apiKey != "" && (
-        <Container>
-          <h1 className="my-4">Movie Search App</h1>
-          <Form inline="true" className="mb-4">
-            <FormControl
-              type="text"
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              placeholder="Search for movies"
-              className="mr-sm-2"
-            />
-            <Button className="mt-3" variant="primary" onClick={searchMovies}>
-              Search
-            </Button>
-          </Form>
+        </h1> */}
 
-          <Row>
-            {movies.map((movie) => (
-              <Col key={movie.imbdID} sm={12} md={6} lg={4} className="mb-4">
-                <Card
-                  onClick={() => getMovieDetails(movie.imbdID)}
-                  style={{ cursor: "pointer" }}
-                >
-                  <Card.Img
-                    variant="top"
-                    style={{ width: "250px", height: "300px" }}
-                    src={movie.Poster}
-                  />
-                  <Card.Body>
-                    <Card.Title>{movie.Title}</Card.Title>
-                    <Card.Text>{movie.Year}</Card.Text>
-                    <Card.Text>{movie.imbdID}</Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-
-          {selectedMovie && (
-            <Col>
-              <Card className="mt-4">
-                <Card.Img variant="top" src={selectedMovie.Poster} />
+      <Container>
+        <h1 className="my-4">Movie Search App</h1>
+        <Form
+          inline="true"
+          onSubmit={(e) => {
+            e.preventDefault();
+            searchMovies();
+          }}
+          className="mb-4"
+        >
+          <FormControl
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search for movies"
+            className="mr-sm-2"
+          />
+          <Button className="mt-3" variant="primary" onClick={searchMovies}>
+            Search
+          </Button>
+        </Form>
+        {selectedMovie && (
+          <Col>
+            <Card className="mt-4">
+              <Card.Img variant="top" src={selectedMovie.Poster} />
+              <Card.Body>
+                <Card.Title>{selectedMovie.Title}</Card.Title>
+                <Card.Text>
+                  <strong>Year:</strong> {selectedMovie.Year}
+                </Card.Text>
+                <Card.Text>
+                  <strong>plot:</strong> {selectedMovie.plot}
+                </Card.Text>
+                <Card.Text>
+                  <strong>Director:</strong> {selectedMovie.Director}
+                </Card.Text>
+                <Card.Text>
+                  <strong>Cast:</strong> {selectedMovie.Actors}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        )}
+        <Row>
+          {movies.map((movie) => (
+            <Col key={movie.imdbID} sm={12} md={6} lg={4} className="mb-4">
+              <Card
+                onClick={() => getMovieDetails(movie.imdbID)}
+                style={{ cursor: "pointer" }}
+              >
+                <Card.Img
+                  variant="top"
+                  style={{ width: "250px", height: "300px" }}
+                  src={movie.Poster}
+                />
                 <Card.Body>
-                  <Card.Title>{selectedMovie.Title}</Card.Title>
-                  <Card.Text>
-                    <strong>Year:</strong> {selectedMovie.Year}
-                  </Card.Text>
-                  <Card.Text>
-                    <strong>plot:</strong> {selectedMovie.lot}
-                  </Card.Text>
-                  <Card.Text>
-                    <strong>Director:</strong> {selectedMovie.Director}
-                  </Card.Text>
-                  <Card.Text>
-                    <strong>Cast:</strong> {selectedMovie.Actors}
-                  </Card.Text>
+                  <Card.Title>{movie.Title}</Card.Title>
+                  <Card.Text>{movie.Year}</Card.Text>
+                  <Card.Text>{movie.imdbID}</Card.Text>
                 </Card.Body>
               </Card>
             </Col>
-          )}
-          <Modal show={showModal} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Error</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>{error}</Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </Container>
-      )}
+          ))}
+        </Row>
+
+        <Modal show={showModal} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Error</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{error}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
     </>
   );
 }
